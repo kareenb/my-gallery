de256a5423742e4cada59a51443d7c231f7e91a2
kareenb <kareenb89@gmail.com>
2018-06-29 21:14:09 +0300

    no bug

--- projs/travel-tip/js/main.js
+++ projs/travel-tip/js/main.js
@@ -91,5 +91,5 @@ function getParameterByName(name, url) {
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+    return +decodeURIComponent(results[2].replace(/\+/g, " "));
 }
